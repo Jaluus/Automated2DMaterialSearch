fdbb9af17e08b9831da4dea10da8d20c8e98f2ce
dudenheini <janlucas.uslu@gmail.com>
2021-07-04 10:23:48 +0200

    Adde Calibration Function

--- FlakeFinder/FlakeFinder/Utils/etc_functions.py
+++ FlakeFinder/FlakeFinder/Utils/etc_functions.py
@@ -4,6 +4,12 @@ A collection of various functions
 import re
 import functools
 import time
+import cv2
+from FlakeFinder.Drivers.Camera_Driver.camera_class import camera_driver_class
+from FlakeFinder.Drivers.Microscope_Driver.microscope_class import (
+    microscope_driver_class,
+)
+from FlakeFinder.Drivers.Motor_Driver.tango_class import motor_driver_class
 
 
 def sorted_alphanumeric(data):
@@ -33,3 +39,47 @@ def timer(func):
         return return_values
 
     return wrapper
+
+
+def calibrate_scope(
+    motor: motor_driver_class,
+    microscope: microscope_driver_class,
+    camera: camera_driver_class,
+):
+
+    MAG_KEYS = {
+        1: "2.5x",
+        2: "5x",
+        3: "20x",
+        4: "50x",
+        5: "100x",
+    }
+
+    while True:
+
+        img = camera.get_image()
+
+        img_small = cv2.resize(img, (960, 600))
+
+        cv2.circle(
+            img_small,
+            (480, 300),
+            10,
+            color=[255, 0, 0],
+            thickness=3,
+        )
+
+        curr_mag = MAG_KEYS[microscope.get_properties()["nosepiece"]]
+
+        cv2.imshow("Calibration Window", img_small)
+        cv2.setWindowTitle("Calibration Window", f"Calibration Window: {curr_mag}")
+
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
+
+        elif key == ord("e"):
+            microscope.rotate_nosepiece_forward()
+
+        elif key == ord("r"):
+            microscope.rotate_nosepiece_backward()
